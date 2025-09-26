39873f2dcfc31b88b8d74c0f789812ef102023aa
Initfan <feeinitfan@gmail.com>
2025-09-26 20:18:25 +0700

    add: identify mood by ai

--- src/components/Describe.tsx
+++ src/components/Describe.tsx
@@ -1,12 +1,32 @@
+import { useRef } from "react";
+import AI from "../utils/ai";
+
 const Describe = () => {
+	const described = useRef<HTMLTextAreaElement>(null);
+
+	const identifyMood = async () => {
+		if (!described.current || !described.current.value) return;
+		alert(described.current.value);
+		const response = await AI.models.generateContent({
+			model: "gemini-2.5-flash",
+			contents: described.current.value,
+			config: {
+				systemInstruction:
+					"Identifikasi mood sesuai cerita yang telah diberikan, berikan respon dalam satu kata berupa mood.",
+			},
+		});
+
+		return { data: response.text };
+	};
+
 	return (
-		<>
+		<div>
 			<textarea
-				name=""
-				id=""
 				placeholder="Describe what you feel..."
+				ref={described}
 			></textarea>
-		</>
+			<button onClick={identifyMood}>identify</button>
+		</div>
 	);
 };
 
---
+++ src/utils/ai.ts
@@ -0,0 +1,7 @@
+import { GoogleGenAI } from "@google/genai";
+
+const AI = new GoogleGenAI({
+	apiKey: import.meta.env.GEMINI_KEY,
+});
+
+export default AI;
