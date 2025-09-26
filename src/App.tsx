7cb5eca8190c5799d020f08f6e5e5c0737077342
Initfan <feeinitfan@gmail.com>
2025-09-26 20:05:01 +0700

    add: indentify mood option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import Mood from "./Mood";
+import { useState } from "react";
+import Mood from "./components/Mood";
+import Describe from "./components/Describe";
 
 const App = () => {
+	const [isDescribe, setIsDescribe] = useState<boolean>(false);
 	return (
 		<div>
 			<h1>Find music that suit your mood</h1>
-			<Mood />
+			{isDescribe ? <Describe /> : <Mood />}
+			<button onClick={() => setIsDescribe((p) => !p)}>
+				{isDescribe ? "Pick mood" : "Desribe what i feel"}
+			</button>
 		</div>
 	);
 };
---
+++ src/components/Describe.tsx
@@ -0,0 +1,13 @@
+const Describe = () => {
+	return (
+		<>
+			<textarea
+				name=""
+				id=""
+				placeholder="Describe what you feel..."
+			></textarea>
+		</>
+	);
+};
+
+export default Describe;
