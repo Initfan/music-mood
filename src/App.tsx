0df6417d01632ce23d2efd19ca5feb3e59fa4b4e
Initfan <feeinitfan@gmail.com>
2025-09-29 20:23:02 +0700

    add: music preference

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import type { mood } from "./utils/types";
 import { db, storage } from "./utils/appwrite";
 import "./App.css";
 import { Query } from "appwrite";
-import Header from "./components/Header";
 import { Card, CardBody } from "@heroui/card";
+import {
+	Button,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
+} from "@heroui/react";
 import Player from "./components/Player";
 import Chart from "./components/Chart";
 
 const App = () => {
 	const [currentMood, setCurrentMood] = useState<mood | null>(null);
 	const [tracks, setTracks] = useState<string[]>([]);
+	const [preference, setPreference] = useState(["local", "international"]);
 
 	useEffect(() => {
 		if (!currentMood) return;
@@ -20,7 +27,10 @@ const App = () => {
 		db.listRows({
 			databaseId: "68da581600322d1917ce",
 			tableId: "music",
-			queries: [Query.contains("mood", currentMood)],
+			queries: [
+				Query.contains("mood", currentMood),
+				Query.contains("preference", preference),
+			],
 		}).then((v) =>
 			v.rows.map((v) =>
 				setTracks((p) => [
@@ -32,11 +42,46 @@ const App = () => {
 				])
 			)
 		);
-	}, [currentMood]);
+	}, [currentMood, preference]);
 
 	return (
 		<div className="space-y-6 w-[90vw] lg:w-[80vw] mx-auto">
-			<Header />
+			<header>
+				<Card radius="sm">
+					<CardBody>
+						<div className="flex justify-between p-3">
+							<h1 className="text-2xl font-semibold">
+								Music by Mood
+							</h1>
+							<Dropdown>
+								<DropdownTrigger>
+									<Button variant="light" color="primary">
+										Music Preference
+									</Button>
+								</DropdownTrigger>
+								<DropdownMenu
+									selectionMode="multiple"
+									closeOnSelect={false}
+									disallowEmptySelection
+									selectedKeys={preference}
+									onSelectionChange={(keys) =>
+										setPreference(
+											Array.from(keys as Set<string>)
+										)
+									}
+								>
+									<DropdownItem key="local">
+										Local
+									</DropdownItem>
+									<DropdownItem key="international">
+										International
+									</DropdownItem>
+								</DropdownMenu>
+							</Dropdown>
+						</div>
+					</CardBody>
+				</Card>
+			</header>
 			<main className="flex flex-col lg:flex-row gap-4 items-start">
 				<div className="lg:w-2/3 w-full space-y-3">
 					<Card className="p-3 space-y-3">
